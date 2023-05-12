978650631b091c48918444e2b56b42446c8f98b5
Chris Schmalz <cschmalz03@gmail.com>
2023-05-11 22:05:38 -0400

    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MenuItem } from './menu-item';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'menu';
+  menuItems: MenuItem[] = [ 
+    { Category: "Pizzas", Name: "Margherita", Price: 10.99 },
+    { Category: "Pizzas", Name: "Pepperoni", Price: 12.99 },
+    { Category: "Pizzas", Name: "Hawaiian", Price: 14.99 },
+    { Category: "Sides", Name: "Garlic Bread", Price: 4.99 },
+    { Category: "Sides", Name: "Chicken Wings", Price: 8.99 },
+    { Category: "Sides", Name: "Caesar Salad", Price: 7.99 },
+    { Category: "Drinks", Name: "Coke", Price: 2.49 },
+    { Category: "Drinks", Name: "Sprite", Price: 2.49 },
+    { Category: "Drinks", Name: "Fanta", Price: 2.49 }
+];
+
+getByCategory(Cat: string): MenuItem[]
+{
+  //console.log(this.menuItems.filter(x => x.Category == Cat))
+  return this.menuItems.filter(x => x.Category == Cat)
+}
+
+Pizzas: MenuItem[] = this.getByCategory("Pizzas");
+Sides: MenuItem[] = this.getByCategory("Sides");
+Drinks: MenuItem[] = this.getByCategory("Drinks");
+
+getUniqueCategory(): string[]{
+  const categories = this.menuItems.map(item => item.Category)
+  //console.log(categories)
+  
+  // return Array.from(new Set(categories));
+  return [...new Set(categories)]
+}
+
 }
